d730b7ef339d3bf72ffc613d653cf5cdb17194bf
kato-0126 <kato.t.0126@gmail.com>
2021-03-20 01:58:50 +0900

    User編集機能実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,7 +1,8 @@
 class ContentsController < ApplicationController
-  def index
+    def index
     @contents = Content.all.order('created_at DESC')
     end
+    
     def create
       @content = Content.new
     end
