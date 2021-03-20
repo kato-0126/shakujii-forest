3ed901625c98fdaa73173e3b256ede2e61eec851
kato-0126 <kato.t.0126@gmail.com>
2021-03-20 23:08:41 +0900

    投稿機能の実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,8 +2,21 @@ class ContentsController < ApplicationController
     def index
     @contents = Content.all.order('created_at DESC')
     end
-    
-    def create
+    def new
       @content = Content.new
     end
+    def create
+      @content = Content.new(content_params)
+      if @content.save
+        redirect_to root_path
+      else
+        @content = Content.new(content_params)
+        render :new
+      end
+    end
+    private
+    def content_params
+      params.require(:content).permit(:image,:title,:explain).merge(user_id:current_user.id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :contents, only:[:index,:new]
+  resources :contents, only:[:index,:new,:create]
   resources :users, only:[:show,:edit,:update]
   root to: 'contents#index'
 end
