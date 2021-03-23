2fc6eaeb063c3129f78fed6c98eec07a674b9d49
kato-0126 <kato.t.0126@gmail.com>
2021-03-23 11:07:00 +0900

    コメント投稿機能の実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -19,6 +19,8 @@ class ContentsController < ApplicationController
     end
 
     def show
+      @comment = Comment.new
+      @comments = @content.comments.includes(:user)
     end
 
     def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :contents
   resources :users, only:[:show,:edit,:update]
   root to: 'contents#index'
+  resources :contents do
+    resources :comments, only: :create
+  end
 end
